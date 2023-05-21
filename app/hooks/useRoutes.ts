561f68b0aea5a94d88df8e548b112323d041dc74
Enrique Sotomayor <sotomaque@gmail.com>
2023-05-20 14:47:58 -1000

    feat: layout for mobile and desktop

---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,39 @@
+import { useMemo } from 'react';
+import { usePathname } from 'next/navigation';
+import { HiChat } from 'react-icons/hi';
+import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
+import { signOut } from 'next-auth/react';
+import useConversation from './useConversation';
+
+const useRoutes = () => {
+  // Hook(s)
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+
+  const routes = useMemo(() => {
+    return [
+      {
+        label: 'Chat',
+        href: '/conversations',
+        icon: HiChat,
+        active: pathname === '/conversations' || !!conversationId,
+      },
+      {
+        label: 'Users',
+        href: '/users',
+        icon: HiUsers,
+        active: pathname === '/users',
+      },
+      {
+        label: 'Sign Out',
+        href: '#',
+        onClick: () => signOut(),
+        icon: HiArrowLeftOnRectangle,
+      },
+    ];
+  }, [pathname, conversationId]);
+
+  return routes;
+};
+
+export default useRoutes;
---
+++ app/users/layout.tsx
@@ -0,0 +1,16 @@
+// server component!
+
+import Sidebar from '../components/sidebar/Sidebar';
+
+export default async function UsersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    // @ts-expect-error Server Component
+    <Sidebar>
+      <div className="h-full">{children}</div>
+    </Sidebar>
+  );
+}
